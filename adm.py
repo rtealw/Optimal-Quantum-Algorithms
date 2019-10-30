884327a8ec1a158762e28e68dc65799776e24060
rtealw <r.teal.witter@gmail.com>
2019-10-29 20:20:48 -0400

    refactor: complete!

--- adm.py
+++ adm.py
@@ -1,5 +1,4 @@
 #http://mpc.zib.de/index.php/MPC/article/viewFile/40/20
-# Algorithm 1: Alternating direction augmented Lagrangian method for SDP
 
 import numpy as np
 from scipy import sparse
@@ -13,11 +12,7 @@ def plainA(As):
         A = np.hstack((A, vec(As[i])))
     return A.T
 
-def mat(x):
-    dimension = int(np.sqrt(len(x)))
-    return x.reshape((dimension, dimension))
-
-def scriptA(As, X, constraintAs, constraints):
+def scriptA(constraints, X):
     traces = []
     for constraint in constraints:
         trace = 0
@@ -30,54 +25,43 @@ def scriptA(As, X, constraintAs, constraints):
     return np.matrix(traces, dtype=np.float32).T
 
 def scriptAStar(A, y):
-    return mat(A.T.dot(y))
+    product = A.T.dot(y)
+    dimension = int(np.sqrt(len(product)))
+    return product.reshape((dimension, dimension))
 
-def nextY(S, X, As, A, C, b, mu, pinvAAt, constraints, constraintAs):
-    matrixPart = -1 * pinvAAt
-    vectorPart = mu * (scriptA(As=As, X=X, constraints=constraints, constraintAs=constraintAs) + -1 * b) + scriptA(As=As, X=S - C, constraints=constraints,  constraintAs=constraintAs)
-    return matrixPart.dot(vectorPart)
+def nextY(S, X, C, b, mu, pinvAAt, constraints):
+    matrix = -1 * pinvAAt
+    vector_a = mu * (scriptA(constraints=constraints, X=X) -b)
+    vector_b = scriptA(constraints=constraints, X=S-C)
+    return matrix.dot(vector_a + vector_b)
 
 def decomposeV(V):
-    eigVals, Q = np.linalg.eig(V) #
-    ordering = (-eigVals).argsort() # puts indices in the descending order
-    #sigma = np.diag(eigVals) #creates a big matrix sigma
- 
-    # to make sure our notation is correct 
-    # we need to ensure that we have sigma+, sigma-
-    #sigma_unordered = np.diag(eigVals)
-    #primeV = Q.dot(sigma_unordered).dot(Q.T)
-    #assert primeV.shape == V.shape
- 
-    sigma = np.diag(eigVals[ordering])
+    eig_vals, Q = np.linalg.eig(V)
+    ordering = (-eig_vals).argsort() 
+    sigma = np.diag(eig_vals[ordering])
     Q = Q[:, ordering] 
-  
-    nNonNeg = sum(eigVals >= 0) #number of non-negative eigenvalues
+    num_non_neg = sum(eig_vals >= 0) #number of non-negative eigenvalues
     
-    sigmaPlus = sigma[:nNonNeg, :nNonNeg]
-    sigmaMinus = sigma[nNonNeg:, nNonNeg:]
-   
+    sigma_plus = sigma[:num_non_neg, :num_non_neg]
     #Q dagger
-    Qplus = Q[:, :nNonNeg] #get columns corresponding to positive eigenvalues
-    #Q double dagger    
-    Qminus = Q[:, nNonNeg:] #get columns corresponding to positive eigenvalues
-
-    return sigmaPlus, sigmaMinus, Qplus, Qminus
+    Q_plus = Q[:, :num_non_neg] #get columns corresponding to positive eigenvalues
+    return sigma_plus, Q_plus
 
 def nextV(C, A, mu, X, y):
     return C - scriptAStar(A=A, y=y) - mu * X
 
 def nextS(V):
-    sigmaPlus, sigmaMinus, Qplus, Qminus = decomposeV(V)
-    stepOne = np.matmul(Qplus, sigmaPlus)
-    return np.matmul(stepOne, Qplus.T)
+    sigma_plus, Q_plus = decomposeV(V)
+    first_product = np.matmul(Q_plus, sigma_plus)
+    return np.matmul(first_product, Q_plus.T)
 
 def nextX(mu, S, V):
     return 1/mu *(S - V)
 
 def simplifyX(X, initial_shape, close_enough=1e-5):
-    idx0 = np.absolute(X - np.zeros((initial_shape, initial_shape), dtype = np.float32)) < close_enough
+    idx0 = np.absolute(X - np.zeros(initial_shape, dtype = np.float32)) < close_enough
     X[idx0] = 0
-    idx1 = np.absolute(X - np.ones((initial_shape, initial_shape), dtype = np.float32)) < close_enough
+    idx1 = np.absolute(X - np.ones(initial_shape, dtype = np.float32)) < close_enough
     X[idx1] = 1
     return X
 
@@ -91,29 +75,25 @@ def getAs(constraints, dimensions):
         As.append(A)
     return As
 
-def solveSDP(As, b, C, constraints, constraintAs, accuracy=1e-4, mu=1, min_iterations=69, max_iterations=420):
-    initial_shape = np.shape(As[0])[0]
-    S = np.eye(initial_shape, dtype=np.float32)
-    X = np.zeros((initial_shape, initial_shape), dtype=np.float32)
+def solveSDP(constraints, b, C, accuracy=1e-5, mu=1, min_iterations=69, max_iterations=420):
+    initial_shape = C.shape
+    S = np.eye(initial_shape[0], dtype=np.float32)
+    X = np.zeros(initial_shape, dtype=np.float32)
     old_z = X[-1, -1]
 
-    As = getAs(constraints, C.shape)
-
+    As = getAs(constraints=constraints, dimensions=initial_shape)
     A = plainA(As)
     pinvAAt = sparse.csr_matrix(np.linalg.pinv(np.matmul(A, A.T)))
-    sparseA = sparse.csr_matrix(A)
+    A = sparse.csr_matrix(A)
 
     for iteration in range(max_iterations):
-        y = nextY(
-            S=S, X=X, As=As, A=sparseA, C=C, b=b, mu=mu, pinvAAt = pinvAAt,
-            constraints=constraints, constraintAs=constraintAs
-        )
-        V = nextV(C=C, A=sparseA, mu=mu, X=X, y=y)
+        y = nextY(S=S, X=X, C=C, b=b, mu=mu, pinvAAt = pinvAAt, constraints=constraints)
+        V = nextV(C=C, A=A, mu=mu, X=X, y=y)
         S = nextS(V)
         X = nextX(mu=mu, S=S, V=V)
         X = simplifyX(X=X, initial_shape=initial_shape)
 
-        # Check if objective value is stagnating
+        # Check if objective value is stabilizing
         if np.absolute(X[-1, -1] - old_z) < accuracy and iteration > min_iterations:
             break
         old_z = X[-1, -1]
--- setup.py
+++ setup.py
@@ -11,87 +11,30 @@ if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
 
-def getA1s(F, dimension, D, n):
-    A1s = []
-    A1Indices = []
-    A1Values = []
-    constraints = []
-    for (y,z) in F:
-        # Construct A_1 that ensures entries corresponding
-        # to bits where y and z strings different sum to 1
-        A_1 = np.zeros((dimension, dimension), dtype = np.float32)
-        constraint = {"V": [], "I" : [], "J" : []}
-#        stringy = D[y]
-#        stringz = D[z]
-        xcoord = n * y
-        ycoord = n * z
-        current_indices = []
-        current_values = []
-        for i in range(n):
-#            xcoord, ycoord = n*y + i, n*z + i
-            current_indices.append((xcoord, ycoord))
-            current_values.append(1)
-            if D[y][i] != D[z][i]:
-                constraint["V"] += [1]
-                constraint["I"] += [xcoord]
-                constraint["J"] += [ycoord]
-                A_1[xcoord, ycoord] = 1
-            xcoord += 1
-            ycoord += 1
-        A1s.append(A_1.T) # transposing because of scriptA
-        A1Indices.append(current_indices)
-        A1Values.append(current_values)
-        constraints.append(constraint)
-#    print("A1Values: {}".format(A1Values))
-#    print("A1 indices: {}".format(A1Indices))
-#    print("constraintsA1: {}".format(constraints))
-#    print(A1s)
-    return A1s, A1Indices, A1Values, constraints
-
 def getA0s(D, n, dimension):
-    A0s = []
-    A0Indices = []
-    A0Values = []
-    count = 0 # add constants rather than multiply
-    slack_starter = n * len(D)
-    slack = n * len(D)
     constraints = []
     for i in range(len(D)):
-        # Construct A0 that ensures input chunk
-        # is less than or equal to z (using slack variables)
+        slack = n * len(D) + i
         constraint = {"V" : [1, -1], "I" : [slack, dimension-1], "J" : [slack, dimension-1]}
-        A0 = np.zeros((dimension, dimension), dtype = np.float32)
-        current_indices = []
-        current_values = []
         for j in range(n):
-            #print("count: {}".format(count))
+            coord = i * n + j
             constraint["V"] += [1]
-            constraint["I"] += [count]
-            constraint["J"] += [count]
-            current_indices.append((count, count))
-            current_values.append(1)
-            A0[count, count] = 1
-            if i * n + j != count:
-                print(count)
-                print(i * n + j)
-            count+=1
+            constraint["I"] += [coord]
+            constraint["J"] += [coord]
         constraints.append(constraint)
-        A0[slack_starter, slack_starter] = 1
-        current_indices.append((slack_starter, slack_starter))
-        current_values.append(1)
-        A0[-1, -1] = -1
-        A0s.append(A0.T) # transposing because of scriptA
-        A0Indices.append(current_indices)
-        current_indices.append((-1, -1))
-        current_values.append(-1)
-        A0Values.append(current_values)
-        slack_starter += 1
-        slack += 1
-#    print("A0Values: {}".format(A0Values))
-#    print("A0 indices: {}".format(A0Indices))
-#    print("constraintsA0: {}".format(constraints))
-#    print(A0s)
-    return A0s, A0Indices, A0Values, constraints
+    return constraints
+
+def getA1s(D, n, F):
+    constraints = []
+    for (y,z) in F:
+        constraint = {"V": [], "I" : [], "J" : []}
+        for i in range(n):
+            if D[y][i] != D[z][i]:
+                constraint["V"] += [1]
+                constraint["I"] += [n*y + i]
+                constraint["J"] += [n*z + i]
+        constraints.append(constraint)
+    return constraints
 
 def getConstraints(D, E):
     F = []               # Cartesian product of inputs with different outputs
@@ -104,21 +47,9 @@ def getConstraints(D, E):
             if E[i] != E[j]:
                 F.append((i,j))
     
-    A0s, A0Indices, A0Values, constraintsA0 = getA0s(D=D, n=n, dimension=dimension)
-    A1s, A1Indices, A1Values, constraintsA1 = getA1s(F=F, dimension=dimension, D=D, n=n)
-    As = []
     constraints = []
-    constraints.extend(constraintsA0)
-    constraints.extend(constraintsA1)
-    As.extend(A0s)
-    As.extend(A1s)
-    constraintIndices = []
-    constraintIndices.extend(A0Indices)
-    constraintIndices.extend(A1Indices)
-    constraintValues = []
-    constraintValues.extend(A0Values)
-    constraintValues.extend(A1Values)
-    constraintAs = {"indices" : constraintIndices, "values" : constraintValues}
+    constraints.extend(getA0s(D=D, n=n, dimension=dimension))
+    constraints.extend(getA1s(D=D, n=n, F=F))
 
     b_0s = np.zeros((len(D), 1), dtype = np.float32) # vector of 0s
     b_1s = np.ones((len(F), 1), dtype = np.float32) # vector of 1s
@@ -126,7 +57,7 @@ def getConstraints(D, E):
 
     C = sparse.csr_matrix(np.zeros((dimension, dimension)))
     C[- 1, - 1] = 1
-    return As, bs, C, constraints, constraintAs
+    return constraints, bs, C
 
 D = ['000','001', '110', '110', '111'] # inputs to Boolean function f
 E = ['0', '1', '1', '1', '1']  # corresponding outputs to f
@@ -158,27 +89,16 @@ def getORWorst(n):
         E.append('1')
     return D, E
 
-def meetsConstraints(As, bs, X, tolerance):
-    for i in range(len(As)):
-        value = np.trace(scipy.linalg.blas.sgemm(1, As[i].T, X))
-        if value < bs[i] - tolerance or value > bs[i] + tolerance:
-            print(value)
-            print(bs[i])
-            return False
-    return True
-
 def wrapSDPSolver(D, E):
-    As, bs, C, constraints, constraintAs = getConstraints(D=D, E=E)
-    X = solveSDP(As=As, b=bs, C=C, constraints = constraints, constraintAs=constraintAs)
-    if not meetsConstraints(As=As, bs=bs, X=X, tolerance=1):
-        raise "X does not meet constraints!"
+    constraints, bs, C = getConstraints(D=D, E=E)
+    X = solveSDP(constraints=constraints, b=bs, C=C)
     return X[-1, -1]
 
-def calculateSDPSolverComplexity(iterations, getDandE, filename):
-    foundVals = []
-    trueVals = []
-    inputSize = []
-    runTime = []
+def calculateSDPSolverComplexity(iterations, getDandE, filename="output"):
+    found_vals = []
+    true_vals = []
+    input_size = []
+    run_time = []
     for i in range(1, iterations+1):
         print("Input size: {}".format(i))
         D, E = getDandE(i)
@@ -187,18 +107,18 @@ def calculateSDPSolverComplexity(iterations, getDandE, filename):
         print("E: {}".format(E))
 
         starting_time = time.time()
-        optVal = wrapSDPSolver(D, E)
+        opt_val = wrapSDPSolver(D, E)
         t = time.time() - starting_time
 
-        print("Obj. Func. Value: {}".format(optVal))
+        print("Obj. Func. Value: {}".format(opt_val))
         print("Run Time: {}  \n".format(t))
 
-        foundVals.extend([optVal.real])
-        trueVals.extend([math.sqrt(i)])
-        inputSize.extend([i])
-        runTime.extend([t])
+        found_vals.extend([opt_val.real])
+        true_vals.extend([math.sqrt(i)])
+        input_size.extend([i])
+        run_time.extend([t])
 
-    resultsDF = pd.DataFrame(data = {'Empirical': foundVals, 'Analytical': trueVals, 'n': inputSize, 'RunTime': runTime})
+    resultsDF = pd.DataFrame(data = {'Empirical': found_vals, 'Analytical': true_vals, 'n': input_size, 'RunTime': run_time})
 
     #write ouput:
     resultsDF.to_csv(index=False, path_or_buf= "./figures/{}.csv".format(filename))
@@ -207,4 +127,4 @@ def calculateSDPSolverComplexity(iterations, getDandE, filename):
 import cProfile
 #cProfile.run("calculateSDPSolverComplexity(6, getORAll, 'michaelTest')", sort = "time")
 
-calculateSDPSolverComplexity(6, getORAll, 'michaelTest')
+calculateSDPSolverComplexity(6, getORAll)
