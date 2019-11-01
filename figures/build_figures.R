70d7d9c873478396f9573df085e1a4858edf592a
rtealw <r.teal.witter@gmail.com>
2019-11-01 12:15:32 -0400

    no redundancy no redundancy

--- figures/build_figures.R
+++ figures/build_figures.R
@@ -2,47 +2,43 @@ library(data.table)
 library(tidyverse)
 library(ggthemes)
 
-# library(devtools)
+library(devtools)
 # devtools::install_github("backlin/treesmapstheorems")
-# library(treesmapstheorems)
+library(treesmapstheorems)
 
 generateFigures <- function(input_name, output_complexity_name, output_time_name, title_description) {
   results <- fread(input_name) %>%
     gather("Method", "Value", -RunTime, -n)
+  
+  custom_theme <- theme(axis.title.y = element_text(color = "black"),
+                        axis.title.x = element_text(color = "black"),
+                        plot.title = element_text(hjust = 0.5, color = "black"),
+                        axis.text.x = element_text(color = "black"),
+                        axis.text.y = element_text(color = "black"),
+                        legend.title = element_text(color = "black"),
+                        legend.text = element_text(color = "black")) +
+                  treesmapstheorems::tmt_theme()
 
   objFunCompPlot <- results %>%
     ggplot(aes(x = n, y = Value, group = Method)) +
     geom_line(aes(color = Method), size = 2)  +
     scale_color_colorblind() +
-    treesmapstheorems::tmt_theme() +
+
     labs(title = paste("Numerical Performance for", title_description),
          x = "Input Size (n)",
          y = "Queries") +
     scale_x_continuous(breaks = 1:max(results$n)) +
-    theme(axis.title.y = element_text(color = "black"),
-          axis.title.x = element_text(color = "black"),
-          plot.title = element_text(hjust = 0.5, color = "black"),
-          axis.text.x = element_text(color = "black"),
-          axis.text.y = element_text(color = "black"),
-          legend.title = element_text(color = "black"),
-          legend.text = element_text(color = "black"))
+    custom_theme
 
 
   runTimePlot <- results %>%
     ggplot(aes(x = n, y = RunTime)) +
     geom_line(size = 2) +
-    treesmapstheorems::tmt_theme() +
     labs(title = paste("Run Time for", title_description),
          x = "Input Size (n)",
          y = "Run Time (s)") +
     scale_x_continuous(breaks = 1:max(results$n)) +
-    theme(axis.title.y = element_text(color = "black"),
-          axis.title.x = element_text(color = "black"),
-          plot.title = element_text(hjust = 0.5, color = "black"),
-          axis.text.x = element_text(color = "black"),
-          axis.text.y = element_text(color = "black"),
-          legend.title = element_text(color = "black"),
-          legend.text = element_text(color = "black"))
+    custom_theme
 
   ggsave(output_complexity_name, plot = objFunCompPlot)
   ggsave(output_time_name, plot = runTimePlot)
