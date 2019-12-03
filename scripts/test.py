bf80241b996b13f10f66b93d1d28e366bb865c38
rtealw <r.teal.witter@gmail.com>
2019-12-03 17:23:09 -0500

    bug fix and better test summary

--- scripts/test.py
+++ scripts/test.py
@@ -2,13 +2,13 @@ import numpy as np
 from quantum_query_optimizer import wrapSDPSolver
 from termcolor import cprint 
 
-def testSDPSolver(n_end=5, accuracy = 2):
+def testSDPSolver(n_end=5, accuracy = 3):
     '''
         Parameters:
             n_end : highest number of bit strings
             accuracy : place to round expected and received to
         Returns:
-           (printed output) : test summary
+           (printed output) : test results for OR
     '''
     all_passed = True
     for n in range(1, n_end + 1):
@@ -25,12 +25,12 @@ def testSDPSolver(n_end=5, accuracy = 2):
             all_passed = False
             cprint("SDP solution failed :(", "red")
             cprint("Expected: {}".format(expected), "green")
-            cprint("Received: {}".format(received), "red")
+            cprint("Received: {}".format(solution['query_complexity']), "red")
 
     if all_passed:
-        cprint("\n All tests passed :)", "green")
+        cprint("All tests passed :)", "green")
     else:
-        cprint("\n Tests failed :(", "red")
+        cprint("Tests failed :(", "red")
 
 if __name__ == '__main__':
     testSDPSolver()
