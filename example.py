274ffee2a9ec2019a1d32048912841570ca94441
rtealw <r.teal.witter@gmail.com>
2019-12-06 14:37:10 -0500

    eps files

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
-import quantum_query_optimizer as qqo
+#import quantum_query_optimizer as qqo
+import sys
+sys.path.append('quantum_query_optimizer')
+import __init__ as qqo
 
 # Example 1
 D = ['00', '01', '10', '11']
@@ -9,10 +12,10 @@ qqo.runSDP(D=D, E=E)
 solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getERandom, n_end=2, n_start=2)
 
 ## Additional Example: Generate Figures
-#all_solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getEOR, n_end=6, n_start=1)
-#qqo.visualizeRuntime(all_solutions, title="Runtime of OR by Input Size for All Inputs", filename="figures/or_all_runtime.png")
-#qqo.visualizeComplexity(all_solutions, title="Complexity of OR by Input Size for All Inputs", filename="figures/or_all_complexity.png")
+all_solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getEOR, n_end=5, n_start=1)
+#qqo.visualizeRuntime(all_solutions, title="Runtime of OR by Input Size for All Inputs", filename="figures/or_all_runtime.eps")
+qqo.visualizeComplexity(all_solutions, title="Complexity of OR by Input Size for All Inputs", filename="figures/or_all_complexity.eps")
 #
 #worst_solutions = qqo.runSDPForN(getD=qqo.getDWorstOR, getE=qqo.getEOR, n_end=20, n_start=1)
-#qqo.visualizeRuntime(solutions, title="Runtime of OR by Input Size for Worse Inputs", filename="figures/temp.png")
-#qqo.visualizeComplexity(worst_solutions, title="Complexity of OR by Input Size for Worse Inputs", filename="figures/or_worst_complexity.png")
+#qqo.visualizeRuntime(solutions, title="Runtime of OR by Input Size for Worse Inputs", filename="figures/or_worst_complexity.eps")
+#qqo.visualizeComplexity(worst_solutions, title="Complexity of OR by Input Size for Worse Inputs", filename="figures/or_worst_complexity.eps")
