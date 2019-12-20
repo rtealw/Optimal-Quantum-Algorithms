fc809c1bceb1e38d89de366bb56adf406ad95b35
rtealw <r.teal.witter@gmail.com>
2019-12-20 12:06:17 -0700

    production example

--- example.py
+++ example.py
@@ -1,16 +1,12 @@
-#import quantum_query_optimizer as qqo
-import sys
-sys.path.append('quantum_query_optimizer')
-import __init__ as qqo
-import numpy as np
+import quantum_query_optimizer as qqo
 
 # Example 1
 D = ['00', '01', '10', '11']
 E = ['0', '1', '1', '1']
-#qqo.runSDP(D=D, E=E)
+qqo.runSDP(D=D, E=E)
 
 # Example 2
-#solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getERandom, n_end=2, n_start=2)
+solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getERandom, n_end=2, n_start=2)
 
 ## Additional Example: Generate Figures
 #all_solutions = qqo.runSDPForN(getD=qqo.getDAll, getE=qqo.getEOR, n_end=5, n_start=1)
